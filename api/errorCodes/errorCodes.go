58fb1cacd4b447720ad1b7c0a422166f43468c97
memez-michalek <michal.lesniowski1@protonmail.com>
2021-01-23 15:29:12 +0100

    added error codes

--- api/errorCodes/errorCodes.go
+++ api/errorCodes/errorCodes.go
@@ -1,9 +1,9 @@
 package errorCodes
 
 var (
-	USERDOESNOTEXIST   = "User does not exist"
-	COULDNOTFINDAPIKEY = "Could not find api key"
-	SHOPDOESNOTEXIST   = "Shop does not exist"
-	COULDNOTBIND       = "COULD NOT BIND TO MODEL"
-	ALREADYLOGGEDIN    = "You are already logged in"
+	USERDOESNOTEXIST    = "User does not exist"
+	COULDNOTFINDAPIKEY  = "Could not find api key"
+	SHOPDOESNOTEXIST    = "Shop does not exist"
+	COULDNOTBIND        = "COULD NOT BIND TO MODEL"
+	USERALREADYLOGGEDIN = "You are already logged in"
 )
