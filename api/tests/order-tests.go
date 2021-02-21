20809b94c910fa1fb75d7c8276d758c8a2852d67
memez-michalek <michal.lesniowski1@protonmail.com>
2021-02-21 01:23:51 +0100

    fixed some bugs

---
+++ api/tests/order-tests.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"api/controllers"
+	"api/models"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestCreateOrderInvalidCreds(t *testing.T) {
+	order := models.Order{
+		Id:       "js",
+		Products: []models.Product{},
+	}
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/createorder", controllers.MakeOrder)
+
+	marshalled, err := json.Marshal(order)
+	if err != nil{
+		t.Errorf("marshalling error")
+	}
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, "/createorder", bytes.NewReader(marshalled)
+	router.ServeHTTP(rec, req)
+	if rec.Code != 400{
+		t.Errorf("error occured")
+	}
+
+}
+func TestCreateOrderInvalidDataType(t *testing.T) {
+	order := "ez"
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/createorder", controllers.MakeOrder)
+
+	marshalled, err := json.Marshal(order)
+	if err != nil{
+		t.Errorf("marshalling error")
+	}
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, "/createorder", bytes.NewReader(marshalled)
+	router.ServeHTTP(rec, req)
+	if rec.Code != 400{
+		t.Errorf("error occured")
+	}
+
+}
+}
+func TestQueryOrderInvalidCreds(t *testing.T) {
+	order := models.Order{
+		Id:       "js",
+		Products: []models.Product{},
+	}
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/queryorder", controllers.MakeOrder)
+
+	marshalled, err := json.Marshal(order)
+	if err != nil{
+		t.Errorf("marshalling error")
+	}
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, "/queryorder", bytes.NewReader(marshalled)
+	router.ServeHTTP(rec, req)
+	if rec.Code != 400{
+		t.Errorf("error occured")
+	}
+
+}
+func TestQueryOrderInvalidDataType(t *testing.T) {
+
+	order := "ez"
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/queryorder", contrllers.QueryOrder)
+
+	marshalled, err := json.Marshal(order)
+	if err != nil{
+		t.Errorf("marshalling error")
+	}
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, "/queryorder", bytes.NewReader(marshalled)
+	router.ServeHTTP(rec, req)
+	if rec.Code != 400{
+		t.Errorf("error occured")
+	}
+
+}
+
+
+}
+
+	func TestDeleteOrderInvalidCreds(t *testing.T) {
+		order := models.Order{
+			Id:       "js",
+			Products: []models.Product{},
+		}
+		gin.SetMode(gin.TestMode)
+		router := gin.Default()
+		router.POST("/deleteorder", controllers.DeleteOrder)
+	
+		marshalled, err := json.Marshal(order)
+		if err != nil{
+			t.Errorf("marshalling error")
+		}
+		rec := httptest.NewRecorder()
+		req, err := http.NewRequest(http.MethodPost, "/deleteorder", bytes.NewReader(marshalled)
+		router.ServeHTTP(rec, req)
+		if rec.Code != 400{
+			t.Errorf("error occured")
+		}
+	
+}
+func TestDeleteOrderInvalidDataType(t *testing.T) {
+	order := "alkfklaafkl"
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/deleteorder", controllers.DeleteOrder)
+
+	marshalled, err := json.Marshal(order)
+	if err != nil{
+		t.Errorf("marshalling error")
+	}
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, "/deleteorder", bytes.NewReader(marshalled)
+	router.ServeHTTP(rec, req)
+	if rec.Code != 400{
+		t.Errorf("error occured")
+	}
+
+}
