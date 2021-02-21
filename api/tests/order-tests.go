3e7e3583bb5b37a50036933076adccb3ea91f09e
memez-michalek <michal.lesniowski1@protonmail.com>
2021-02-21 01:27:18 +0100

    fixed minor bugs

--- api/tests/order-tests.go
+++ api/tests/order-tests.go
@@ -1,4 +1,4 @@
-package main
+package controllers
 
 import (
 	"api/controllers"
@@ -22,13 +22,13 @@ func TestCreateOrderInvalidCreds(t *testing.T) {
 	router.POST("/createorder", controllers.MakeOrder)
 
 	marshalled, err := json.Marshal(order)
-	if err != nil{
+	if err != nil {
 		t.Errorf("marshalling error")
 	}
 	rec := httptest.NewRecorder()
-	req, err := http.NewRequest(http.MethodPost, "/createorder", bytes.NewReader(marshalled)
+	req, err := http.NewRequest(http.MethodPost, "/createorder", bytes.NewReader(marshalled))
 	router.ServeHTTP(rec, req)
-	if rec.Code != 400{
+	if rec.Code != 400 {
 		t.Errorf("error occured")
 	}
 
@@ -40,18 +40,18 @@ func TestCreateOrderInvalidDataType(t *testing.T) {
 	router.POST("/createorder", controllers.MakeOrder)
 
 	marshalled, err := json.Marshal(order)
-	if err != nil{
+	if err != nil {
 		t.Errorf("marshalling error")
 	}
 	rec := httptest.NewRecorder()
-	req, err := http.NewRequest(http.MethodPost, "/createorder", bytes.NewReader(marshalled)
+	req, err := http.NewRequest(http.MethodPost, "/createorder", bytes.NewReader(marshalled))
 	router.ServeHTTP(rec, req)
-	if rec.Code != 400{
+	if rec.Code != 400 {
 		t.Errorf("error occured")
 	}
 
 }
-}
+
 func TestQueryOrderInvalidCreds(t *testing.T) {
 	order := models.Order{
 		Id:       "js",
@@ -62,13 +62,13 @@ func TestQueryOrderInvalidCreds(t *testing.T) {
 	router.POST("/queryorder", controllers.MakeOrder)
 
 	marshalled, err := json.Marshal(order)
-	if err != nil{
+	if err != nil {
 		t.Errorf("marshalling error")
 	}
 	rec := httptest.NewRecorder()
-	req, err := http.NewRequest(http.MethodPost, "/queryorder", bytes.NewReader(marshalled)
+	req, err := http.NewRequest(http.MethodPost, "/queryorder", bytes.NewReader(marshalled))
 	router.ServeHTTP(rec, req)
-	if rec.Code != 400{
+	if rec.Code != 400 {
 		t.Errorf("error occured")
 	}
 
@@ -78,44 +78,41 @@ func TestQueryOrderInvalidDataType(t *testing.T) {
 	order := "ez"
 	gin.SetMode(gin.TestMode)
 	router := gin.Default()
-	router.POST("/queryorder", contrllers.QueryOrder)
+	router.POST("/queryorder", controllers.QueryOrder)
 
 	marshalled, err := json.Marshal(order)
-	if err != nil{
+	if err != nil {
 		t.Errorf("marshalling error")
 	}
 	rec := httptest.NewRecorder()
-	req, err := http.NewRequest(http.MethodPost, "/queryorder", bytes.NewReader(marshalled)
+	req, err := http.NewRequest(http.MethodPost, "/queryorder", bytes.NewReader(marshalled))
 	router.ServeHTTP(rec, req)
-	if rec.Code != 400{
+	if rec.Code != 400 {
 		t.Errorf("error occured")
 	}
 
 }
 
+func TestDeleteOrderInvalidCreds(t *testing.T) {
+	order := models.Order{
+		Id:       "js",
+		Products: []models.Product{},
+	}
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/deleteorder", controllers.DeleteOrder)
 
-}
+	marshalled, err := json.Marshal(order)
+	if err != nil {
+		t.Errorf("marshalling error")
+	}
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodPost, "/deleteorder", bytes.NewReader(marshalled))
+	router.ServeHTTP(rec, req)
+	if rec.Code != 400 {
+		t.Errorf("error occured")
+	}
 
-	func TestDeleteOrderInvalidCreds(t *testing.T) {
-		order := models.Order{
-			Id:       "js",
-			Products: []models.Product{},
-		}
-		gin.SetMode(gin.TestMode)
-		router := gin.Default()
-		router.POST("/deleteorder", controllers.DeleteOrder)
-	
-		marshalled, err := json.Marshal(order)
-		if err != nil{
-			t.Errorf("marshalling error")
-		}
-		rec := httptest.NewRecorder()
-		req, err := http.NewRequest(http.MethodPost, "/deleteorder", bytes.NewReader(marshalled)
-		router.ServeHTTP(rec, req)
-		if rec.Code != 400{
-			t.Errorf("error occured")
-		}
-	
 }
 func TestDeleteOrderInvalidDataType(t *testing.T) {
 	order := "alkfklaafkl"
@@ -124,13 +121,13 @@ func TestDeleteOrderInvalidDataType(t *testing.T) {
 	router.POST("/deleteorder", controllers.DeleteOrder)
 
 	marshalled, err := json.Marshal(order)
-	if err != nil{
+	if err != nil {
 		t.Errorf("marshalling error")
 	}
 	rec := httptest.NewRecorder()
-	req, err := http.NewRequest(http.MethodPost, "/deleteorder", bytes.NewReader(marshalled)
+	req, err := http.NewRequest(http.MethodPost, "/deleteorder", bytes.NewReader(marshalled))
 	router.ServeHTTP(rec, req)
-	if rec.Code != 400{
+	if rec.Code != 400 {
 		t.Errorf("error occured")
 	}
 
