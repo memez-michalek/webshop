4a0c7922ff70f5bfa21c645f0d06ff1fb93cf1cf
memez-michalek <michal.lesniowski1@protonmail.com>
2021-02-23 16:13:08 +0100

    added frontend, fixed some bugs

--- templateBackend/controllers/sessionControllers.go
+++ templateBackend/controllers/sessionControllers.go
@@ -1,12 +1,17 @@
 package controllers
 
-import ("github.com/gin-gonic/gin"
+import (
+	"bytes"
+	"encoding/json"
+	"log"
 
-"templateBackend/models"
- "net/http"
-"templateBackend/handlers")
+	"github.com/gin-gonic/gin"
 
-func MainSiteController(c *gin.Context){
+	"net/http"
+	"templateBackend/models"
+)
+
+func MainSiteController(c *gin.Context) {
 	marshalled, err := json.Marshal(helpers.SHOPAPIWEBTOKEN)
 	resp, err := http.Post("/api/main", bytes.NewReader(marshalled))
 	switch {
@@ -20,73 +25,73 @@ func MainSiteController(c *gin.Context){
 		c.JSON(200, resp.Body)
 	}
 }
-func LoginController(c *gin.Context){
-	var(
-		login := new(models.User)
+func LoginController(c *gin.Context) {
+	var (
+		user = new(models.User)
 	)
-	err := c.ShouldBind(&login)
+	err := c.ShouldBind(&user)
 	if err != nil {
 		log.Print("access denied")
 		c.JSON(403, err)
-	}else{
-		err := login.CheckLoginData()
+	} else {
+		err := user.CheckLoginData()
 		if err != nil {
 			log.Print("login data wasnt verified: ", err)
 			c.JSON(404, err)
-		}else{
-			_ , exists := models.VALIDJWTTOKENS[login.Email]
-			if exists{
+		} else {
+			_, exists := models.VALIDJWTTOKENS[user.Email]
+			if exists {
 				log.Print("you are already logged in")
 				c.JSON(403, "already logged in")
 			}
-			token, err  := helpers.CreateToken(login.Email, login.Password)	
-			if err != nil{
+			token, err := helpers.CreateToken(user.Email, user.Password)
+			if err != nil {
 				log.Print("could not create login token", err)
 				c.JSON(403, err)
 			}
-			models.VALIDJWTTOKENS[login.Email] = token
+			models.VALIDJWTTOKENS[user.Email] = token
 			c.JSON(200, "logged in")
 
 		}
 	}
 
 }
-func RegisterController(c *gin.Context){
-	var(
-		user := new(models.User)
+func RegisterController(c *gin.Context) {
+	var (
+		user = new(models.User)
 	)
 	err := c.ShouldBind(&user)
-	if err != nil{
+	if err != nil {
 		log.Print("could not bind to model")
 		c.JSON(400, "could not bind")
-	}else{
-		_ , exists := models.VALIDJWTTOKENS[user.Email]
-			if exists{
-				log.Print("you are already logged in")
-				c.JSON(403, "already logged in")
-			}
-	
+	} else {
+		_, exists := models.VALIDJWTTOKENS[user.Email]
+		if exists {
+			log.Print("you are already logged in")
+			c.JSON(403, "already logged in")
+		}
+
 		err := user.RegisterUser()
-		if err != nil{
+		if err != nil {
 			log.Print("bad user credentials", err)
 			c.JSON(403, "could not log in")
 		}
 		token, err := helpers.CreateToken(user.Email, user.Password)
-		if err != nil{
+		if err != nil {
 			log.Print("could not create token")
 		}
 		models.VALIDJWTTOKENS[user.Email] = token
 		c.JSON(200, "user registered")
 
-	}	
+	}
 
 }
-func LogoutController(c *gin.Context){
-	var(
-		user := models.VerifiedUser
+func LogoutController(c *gin.Context) {
+	var (
+		user = new(models.verified)
 	)
-	email, password, err := helpers.GetTokenValue(token)
-	if err != nil{
+	email, password, err := helpers.GetTokenValue(user.Token)
+	if err != nil {
 		log.Print("could not get the value of token")
 		c.JSON(401, "could not get value of token")
 	}
